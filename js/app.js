310d65fc18722e8cd8bc922043912df08e3c23a6
izPanda <zachashaner@gmail.com>
2021-03-22 22:59:59 -0700

    Initializing VueJS

---
+++ js/app.js
@@ -0,0 +1,14 @@
+const app = Vue.createApp({
+    data() {
+      return {
+        p1Sponsor: 'hpt',
+        p1Name: 'rickles',
+        p1Score: '0',
+        p2Sponsor:'izg',
+        p2Name: 'panda',
+        p2Score: '0',
+      }
+    },
+  })
+  
+  app.mount('#app')
